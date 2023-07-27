100983f1664ea72b62e6665ba68a065d0bb25ff0
ahlinder <joelahlinder@hotmail.com>
2023-07-27 13:23:04 +0200

    add buttons as player-input instead of text

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const playingObject = ["rock", "paper", "scissors"];
+let currentPlayerScore = 0;
+let currentComputerScore = 0;
+const buttons = document.querySelectorAll('button');
 
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        oneRound(button.className, getComputerChoice());
+    })
+})
 /* array with rock, paper, scissors
     randmize a number between 0 and 2
     select the string at that position in the array*/
@@ -12,80 +20,66 @@ function getComputerChoice(){
     check if input was correct, otherwise ask for input again
     call another function that checks who won and presents message*/
 
+
+function updateScore(playerScore, computerScore){
+    let scoreBoard = document.querySelector('#result');
+    currentComputerScore = currentComputerScore + computerScore;
+    currentPlayerScore = currentPlayerScore + playerScore;
+    if(currentComputerScore < 5){
+        if(currentPlayerScore < 5){
+            scoreBoard.textContent = currentPlayerScore + " - " + currentComputerScore;
+        }
+        else{
+            scoreBoard.textContent = "player won with 5 against " + currentComputerScore;
+            currentComputerScore = 0;
+            currentPlayerScore = 0;
+        }
+    }
+
+    else{
+        scoreBoard.textContent = "computer won with 5 against " + currentPlayerScore;
+        currentComputerScore = 0;
+        currentPlayerScore = 0;
+    }
+    
+}
+/* Called once a button is clicked with playerSelection as the button selected*/
 function oneRound(playerSelection, computerSelection){
+    let playerScore = 0;
+    let computerScore = 0;
     if(playerSelection === computerSelection){
         console.log("Both selected " + playerSelection + ", it was a draw.");
-        return "draw";
+        
     }
     else if(playerSelection === "rock"){
         if(computerSelection === "paper"){
             console.log("Computer won with " + computerSelection + " against " + playerSelection);
-            return "computer";
+            computerScore = 1;
         }
         else{
             console.log("Player won with " + playerSelection+ " against " + computerSelection);
-            return "player";
+            playerScore = 1;
         }
      }
      else if(playerSelection === "paper"){
         if(computerSelection === "scissors"){
             console.log("Computer won with " + computerSelection + " against " + playerSelection);
-            return "computer";
+            computerScore = 1;
         }
         else{
             console.log("Player won with " + playerSelection+ " against " + computerSelection);
-            return "player";
+            playerScore = 1;
         }
      }
      else if(playerSelection === "scissors"){
         if(computerSelection === "rock"){
             console.log("Computer won with " + computerSelection + " against " + playerSelection);
-            return "computer";
+            computerScore = 1;
         }
         else{
             console.log("Player won with " + playerSelection+ " against " + computerSelection);
-            return "player";
+            playerScore = 1;
         }
      }
+     updateScore(playerScore, computerScore);
 }
-
-/*Use prompt to let user write input
-if input isn't correct ask for input again*/
-function getUserInput(){
-    let input = "";
-    while(!playingObject.includes(input)){
-        if(input !== ""){
-            console.log("You can only select:'rock', 'paper' or 'scissors' ")
-        }
-        input = prompt("Select 'rock', 'paper' or 'scissors'.");
-        input = input.toLowerCase();
-    }
-    return input;
-}
-
-/* create scores for user and pc
-    loop over game until one of the scores reach 5 points,
-    then exit loop*/
-function game(){
-    let userScore = 0;
-    let computerScore = 0;
-    while(computerScore !== 5 && userScore !== 5){
-        let playerSelection = getUserInput();
-        let winner = oneRound(playerSelection, getComputerChoice());
-        if(winner === "player"){
-            userScore++;
-        }
-        else if(winner === "computer"){
-            computerScore++;
-        }
-    }
-
-    if(userScore === 5){
-        console.log("Player won with " + userScore + " against " + computerScore + "!");
-    }
-    else{
-        console.log("Computer won with " + computerScore + " against " + userScore + "!");
-    }
-}
-
-game();
